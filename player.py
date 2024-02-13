Ball collision with the 'ground' fixed, now implementing rotation of the polygon generated in the center of the screen. Currently working on rotation of the polygon, and I'm trying to decide between using pygame's Vector2 class and numpy's arrays for storage and calculations with vector data. I believe that the former is more intuitive, allows me to approach programming in a more 'Pythonic' way, and the latter should (if utilized correctly) improve performance.  Next up, implementing collision detection using Sprites and Masks.

player.py
@@ -1,62 +1,68 @@
 '''
     _summary_: Game object class for the player in hexkeys
 '''
-from hexkey_utils import *
+from utils import *
 
-GRAVITY = np.array([0, 2])
+GRAVITY = np.array([0, 5])
 
-class Ball():
-    def __init__(self, mass, radius, color):
-        # pygame.sprite.Sprite.__init__(self)
-        
-        self.mass = mass
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, color):
+        # print(str(super()), self.groups())
+        super().__init__()
         self.radius = radius
         self.color = color
+        self.position = CENTER
+        # self.mass = mass
         
-        self.position = np.array([SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2])
         self.velocity = np.array([0.0, 0.0])
         self.acceleration = np.array([0.0, 0.0])
         
         self.keys_held = []
         
-        # self.image = pygame.Surface((self.radius * 2, self.radius * 2))
-        # self.image.fill((255, 255, 255))
-        # self.image.set_colorkey((255, 255, 255))
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2))
+        self.image.fill((0, 0, 0))
+        self.image.set_colorkey((0, 0, 0))
         
-        pygame.draw.circle(pygame.display.get_surface(), self.color, self.position.astype(int), self.radius)
-        # self.rect = self.image.get_rect()
+        pygame.draw.circle(self.image, self.color, [self.radius, self.radius], self.radius)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position.astype(int) - self.radius
         
+    def draw(self, surface):
+        self.rect.topleft = self.position - self.radius
+        surface.blit(self.image, self.rect.topleft) 
+    
     def update(self):
         self.acceleration = GRAVITY
         self.velocity += self.acceleration
-        self.position += self.velocity
         
-        if len(self.keys_held) > 0:
-            # TODO: Adjust the velocity accordingly
-            print('Movements to apply: ' + str(self.keys_held))
+        # Applying any user input for movement 
+        if self.keys_held.count('left'):
+            self.velocity[0] += -0.5  # Left is -X
+        if self.keys_held.count('right'):
+            self.velocity[0] += 0.5   # Right is +X
+        self.position += self.velocity
             
         # Checking if hit left wall, correct position to be within the wall and bounce off
         if self.position[0] - self.radius < 0:
             self.position[0] = self.radius
-            self.velocity[0] *= -0.3    # Negative to reverse direction, < 1 to simulate loss of momentum
+            self.velocity[0] *= -0.2    # Negative to reverse direction, < 1 to simulate loss of momentum
             
         # Checking if hit right wall
         if self.position[0] + self.radius >= SCREEN_WIDTH:
             self.position[0] = SCREEN_WIDTH - self.radius
-            self.velocity[0] *= -0.9    # Bounce off with a dampening effect
+            self.velocity[0] *= -0.2    # Bounce off with a dampening effect
             
         # Checking if hit top wall
         if self.position[1] - self.radius < 0:
             self.position[1] = self.radius
-            self.velocity[1] *= -0.3   
+            self.velocity[1] *= -0.9   
 
         # Checking if hit bottom wall
         if self.position[1] + self.radius >= SCREEN_HEIGHT:
             self.position[1] = SCREEN_HEIGHT - self.radius
             self.velocity[1] *= -0.9
         
-        pygame.draw.circle(pygame.display.get_surface(), self.color, self.position.astype(int), self.radius)
-        # self.rect.move(self.position.astype(float))
+        self.rect.topleft = self.position.astype(int) - self.radius
         
     def add_key_held(self, key):
         self.keys_held.append(key)

polygon.py
@@ -1,5 +1,5 @@
-import pygame, random
-from hexkey_utils import *
+import pygame
+from utils import *
 
 RING_PADDING = 50
 RING_SIDES = {
@@ -11,39 +11,53 @@ RING_SIDES = {
     'down_left'
 }
 RING_PALLETE = {
-    'white': (255, 255, 255),
+    'white': (250, 250, 250),
     'pink': (201, 93, 177),
     'light-purple': (137, 100, 187),
     'blue': (157, 169, 214),
     'light-blue': (170, 224, 241),
     'cyan': (144, 239, 240)
 }
-SIZE = (300, 300)
+
+def rotate_vector(angle, x, y):
+    '''
+    After rotation the vector is noted by
+    x' = x * cos(theta) - y * sin(theta)
+    y' = x * sin(theta) + y * cos(theta)
+    '''
+    a = np.cos(angle)
+    b = np.sin(angle)
+    R = np.matrix((a, -b), (b, a))
+    return np.matmul(((a, -b), (b, a)), (x, y))
 
 class Polygon(pygame.sprite.Sprite):
-    '''Polygon ring rotates using  A (counterclockwise) and D (clockwise).
+    '''Polygon ring rotates using  Q (counterclockwise) and E (clockwise).
 
     Args:
         pygame (pygame.sprite.Sprite): base class
     '''
     
-    
-    def __init__(self, radius, N=4, inner_ring=None):
-        pygame.sprite.Sprite.__init__(self)
-        self.N = N            # number of sides
-        self.center = CENTER
+    def __init__(self, radius, color, N):
+        # print(str(super()), self.groups())
+        super().__init__()
         self.radius = radius    
-        # self.active = False     # active ==> ball currently inside
+        self.color = color
+        self.position = np.array([SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2])
+        self.N = N            # number of sides
+        
+        self.active = False     # active ==> ball currently inside
+        self.keys_held = []
+        
         self.theta = self.get_theta()
         self.vertices = self.get_vertices()
-        # self.lines = 
-        
-        # self.base_surface = pygame.display.get_surface()
-        self.image = pygame.Surface(SIZE, pygame.SRCALPHA, 32)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (self.center.x - self.radius, self.center.y - self.radius)
-        self.mask = pygame.mask.from_surface(self.image)  # get the surface from the area bounded by the shape for the mask
-        
+        self.vectors = self.get_vectors()
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2))
+        self.image.fill((0, 0, 0))
+        self.image.set_colorkey((0, 0, 0))
+        pygame.draw.lines(self.image, self.color, True, self.vertices)
+        self.rect = self.image.get_rect(center = self.position)
+        # get the surface from the area bounded by the shape for the mask
+        self.mask = pygame.mask.from_surface(self.image)  
     
     def get_theta(self):
         return (2 * np.pi) / self.N
@@ -52,30 +66,44 @@ class Polygon(pygame.sprite.Sprite):
         vertices = []
         for i in range(self.N):
             theta = i * self.theta
-            x = (self.radius * np.cos(theta)) + self.center.x
-            y = (self.radius * np.sin(theta)) + self.center.y
-            vertices.append(pygame.Vector2(x, y))
+            x = (self.radius * np.cos(theta)) + self.radius
+            y = (self.radius * np.sin(theta)) + self.radius
+            vertices.append(np.array((x, y)))
         return vertices
 
-    def draw(self):
-        # TODO Create a transparent surface to draw on
-        # new_surface = pygame.Surface.subsurface(self.base_surface, self.center, SIZE)
-        # new_surface = pygame.Surface(SIZE, pygame.SRCALPHA, 32)
-        
-        # TODO Draw the polygon onto the transparent surface
-        pygame.draw.aalines(self.image, random.choice(list(RING_PALLETE.values())), False, self.vertices)
-        
-        
+    def get_vectors(self):
+        vectors = []
+        for p in self.vertices:
+            pygame.
+            vectors.append(self.position.astype(float), p)
+        return vectors
+    
+    def draw(self, surface):
+        blit_position = self.position - self.radius
+        surface.blit(self.image, blit_position.astype(int))
         
     def update(self):
-        pygame.draw.aalines(pygame.display.get_surface(), random.choice(list(RING_PALLETE.values())), True, self.vertices)
-        
-        
+        # self.rect.x, self.rect.y = self.position.astype(int) - self.radius
+        self.rect = pygame.draw.lines(self.image, self.color, True, self.vertices)
+        self.rect.topleft = self.position.astype(int) - self.radius
         
     def cw_rotate(self):
-        pass
+        # new_vertices = []
+        # for p in self.vertices:
+        #     result = np.array((0.0, 0.0))
+        #     result = rotate_vector(-1.0 * self.theta / 2.0, p[0], p[1])
+        #     new_vertices.append(result)
+        # self.vertices = new_vertices
+        angle = self.theta / 2.0
+        
+        
         
     def ccw_rotate(self):
-        #TODO
+        # new_vertices = []
+        # for p in self.vertices:
+        #     result = np.array((0.0, 0.0))
+        #     result = rotate_vector(self.theta / 2.0, p[0], p[1])
+        #     new_vertices.append(result)
+        # self.vertices = new_vertices
+            
         pass
-    