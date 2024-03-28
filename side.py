a2f8bb7f43ad17540be44102d80983936d28ab49
Johnny Wannamaker <j.wannamaker19@gmail.com>
2024-03-27 23:53:33 -0700

    Did a lot of pruning of the codebase. Felt great. Should have a nice fresh start tomorrow on everything. having a great time now! Things are going to be looking great in time for my presentation 4/14/24

--- side.py
+++
@@ -1,6 +0,0 @@
-'''
-Represents one side of a Polygon. Using for better organization of data.
-
-Inherits Sprite in order to use collision of masks; Kill individual sides
-instead of the whole polygon.
-'''
--- test.py
+++
@@ -1,4 +0,0 @@
-from collections import deque
-from polygon import Side, Polygon
-
-ring_a = Polygon()
